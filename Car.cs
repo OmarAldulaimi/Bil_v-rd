65df0974582a107ab767e7a109c66960d95a4a0c
Omar <omar.aldulaimi2@hotmail.com>
2019-12-28 11:41:48 +0100

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bil_vård
+{
+    class Car
+    {
+        private int id;
+        private string carModel;
+        private string regNbr;
+        private string friText;
+       
+
+        public Car() { }
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string CarModel
+        {
+            get { return carModel; }
+            set { carModel = value; }
+        }
+
+        public string RegNbr
+        {
+            get { return regNbr; }
+            set { regNbr = value; }
+        }
+        public string FriText
+        {
+            get { return friText; }
+            set { friText = value; }
+        }
+    }
+}
---
+++ Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bil_vård
+{
+    class Controller
+    {
+        private List<Car> carList;
+        
+
+
+    public Controller()
+    {
+            this.carList = new List<Car>();
+            this.carList.Add(new Car() { Id = 1, CarModel = "KIA", RegNbr = "FHO236", FriText = "Helrekond" });
+            foreach (Car car in carList)
+            {
+                Console.WriteLine($"Car: {car.FriText}");
+            }
+
+        }
+    
+}
+}
